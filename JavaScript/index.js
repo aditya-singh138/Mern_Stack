4ffcbac9ec5cca5d588642f6741cc430798bf50f
Aditya Singh <adisingh5005@gmail.com>
2025-10-17 12:41:36 +0530

    intial commit

---
+++ JavaScript/index.js
@@ -0,0 +1,15 @@
+// let age=10;
+// if(age>18){
+//     console.log("vote");
+// }
+// else{
+//     console.log("not");
+// }
+
+let n= prompt("enter the no:");
+if(n%5==0){
+    console.log("multiple of 5");
+}
+else{
+    console.log("not0");
+}
---
+++ JavaScript/string.js
@@ -0,0 +1,42 @@
+// let n=100;
+// for(let i=0;i<=100;i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+
+// let str="heLlo";
+// console.log(str);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+
+// let str="   hello world   ";
+// console.log(str.trim());
+
+
+// let s1="hello ";
+// let s2="world";
+// let s3= s1+s2;
+// console.log(s3);
+
+
+// let str="welcome";
+// console.log(str.slice(1,4));
+
+
+// let str="welcome";
+// console.log(str.replace("e","a"));
+
+
+
+// let str="javascript";
+// console.log(str[0]);
+// console.log(str.charAt(2));
+
+
+
+let str=prompt("input do jaldi: ");
+let ans="@"+str+"13";
+console.log(ans);
